c1ba25a4426079f117ae9dae5d4867779c829e6a
Abirami6493 <megalasounder69@gmail.com>
2025-04-25 21:57:59 +0530

    1st Tester Abirami

---
+++ src/test/java/com/omrbranch/git/Git.java
@@ -0,0 +1,32 @@
+package com.omrbranch.git;
+
+public class Git {
+	public void method1() {
+		
+        System.out.println("Method1");
+	}
+public void method2() {
+		
+        System.out.println("Method2");
+	}
+public void method3() {
+	
+    System.out.println("Method3");
+}
+
+
+	
+	public static void main(String[] args) {
+        
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
