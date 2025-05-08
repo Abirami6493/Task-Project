a13765eba15f2ca957d4444b40e67c1f848ef15d
Abirami6493 <abirami64933@gmail.com>
2025-05-08 12:13:51 +0530

    message

--- src/test/java/com/omrbranch/git/Git.java
+++ src/test/java/com/omrbranch/git/Git.java
@@ -13,6 +13,23 @@ public void method3() {
 	
     System.out.println("Method3");
 }
+public void method4() {
+	
+    System.out.println("Method4");
+}
+public void method5() {
+	
+    System.out.println("Method5");
+}
+public void method6() {
+	 System.out.println("Method6");
+
+}
+public void method7() {
+	System.out.println("Method7");
+
+	
+}
 
 
 	
